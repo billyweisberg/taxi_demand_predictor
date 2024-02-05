6bf6ea0e60b23e5cc7e355ecb9573380a767f63a
Billy Weisberg <billy@energyapps.com>
2024-02-04 20:56:10 -0700

    update config 2

--- src/config.py
+++ src/config.py
@@ -15,8 +15,8 @@ except:
     raise Exception('Create an .env file on the project root with the HOPSWORKS_API_KEY')
 
 # TODO: remove FEATURE_GROUP_NAME and FEATURE_GROUP_VERSION, and use FEATURE_GROUP_METADATA instead
-# FEATURE_GROUP_NAME = 'time_series_hourly_feature_group'
-# FEATURE_GROUP_VERSION = 4
+FEATURE_GROUP_NAME = 'time_series_hourly_feature_group'
+FEATURE_GROUP_VERSION = 5
 FEATURE_GROUP_METADATA = FeatureGroupConfig(
     name='time_series_hourly_feature_group',
     version=5,
@@ -27,8 +27,8 @@ FEATURE_GROUP_METADATA = FeatureGroupConfig(
 )
 
 # TODO: remove FEATURE_VIEW_NAME and FEATURE_VIEW_VERSION, and use FEATURE_VIEW_METADATA instead
-# FEATURE_VIEW_NAME = 'time_series_hourly_feature_view'
-# FEATURE_VIEW_VERSION = 3
+FEATURE_VIEW_NAME = 'time_series_hourly_feature_view'
+FEATURE_VIEW_VERSION = 5
 FEATURE_VIEW_METADATA = FeatureViewConfig(
     name='time_series_hourly_feature_view',
     version=5,
@@ -36,11 +36,11 @@ FEATURE_VIEW_METADATA = FeatureViewConfig(
 )
 
 MODEL_NAME = "taxi_demand_predictor"
-MODEL_VERSION = 5
+MODEL_VERSION = 3
 
 # added for monitoring purposes
 # TODO remove FEATURE_GROUP_MODEL_PREDICTIONS and use FEATURE_GROUP_PREDICTIONS_METADATA instead
-# FEATURE_GROUP_MODEL_PREDICTIONS = 'model_predictions_feature_group'
+FEATURE_GROUP_MODEL_PREDICTIONS = 'model_predictions_feature_group'
 FEATURE_GROUP_PREDICTIONS_METADATA = FeatureGroupConfig(
     name='model_predictions_feature_group',
     version=5,
@@ -50,7 +50,7 @@ FEATURE_GROUP_PREDICTIONS_METADATA = FeatureGroupConfig(
 )
 
 # TODO remove FEATURE_VIEW_MODEL_PREDICTIONS and use FEATURE_VIEW_PREDICTIONS_METADATA instead
-# FEATURE_VIEW_MODEL_PREDICTIONS = 'model_predictions_feature_view'
+FEATURE_VIEW_MODEL_PREDICTIONS = 'model_predictions_feature_view'
 FEATURE_VIEW_PREDICTIONS_METADATA = FeatureViewConfig(
     name='model_predictions_feature_view',
     version=5,
