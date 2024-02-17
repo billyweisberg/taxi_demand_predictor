6f8d9ca31243afa6d2192fb53a994beabd88e834
Billy Weisberg <billy@energyapps.com>
2024-02-16 18:05:39 -0700

    add feature store backfill

--- src/config.py
+++ src/config.py
@@ -16,10 +16,10 @@ except:
 
 # TODO: remove FEATURE_GROUP_NAME and FEATURE_GROUP_VERSION, and use FEATURE_GROUP_METADATA instead
 FEATURE_GROUP_NAME = 'time_series_hourly_feature_group'
-FEATURE_GROUP_VERSION = 6
+FEATURE_GROUP_VERSION = 7
 FEATURE_GROUP_METADATA = FeatureGroupConfig(
     name='time_series_hourly_feature_group',
-    version=5,
+    version=7,
     description='Feature group with hourly time-series data of historical taxi rides',
     primary_key=['pickup_location_id', 'pickup_ts'],
     event_time='pickup_ts',
@@ -28,7 +28,7 @@ FEATURE_GROUP_METADATA = FeatureGroupConfig(
 
 # TODO: remove FEATURE_VIEW_NAME and FEATURE_VIEW_VERSION, and use FEATURE_VIEW_METADATA instead
 FEATURE_VIEW_NAME = 'time_series_hourly_feature_view'
-FEATURE_VIEW_VERSION = 6
+FEATURE_VIEW_VERSION = 7
 FEATURE_VIEW_METADATA = FeatureViewConfig(
     name='time_series_hourly_feature_view',
     version=5,
@@ -37,17 +37,17 @@ FEATURE_VIEW_METADATA = FeatureViewConfig(
 
 # MODEL_NAME = "taxi_demand_predictor"
 MODEL_NAME = "taxi_demand_predictor_next_hour"
-MODEL_VERSION = 3
+MODEL_VERSION = 7
 
 # added for monitoring purposes
 # TODO remove FEATURE_GROUP_MODEL_PREDICTIONS and use FEATURE_GROUP_PREDICTIONS_METADATA instead
 FEATURE_GROUP_MODEL_PREDICTIONS = 'model_predictions_feature_group'
 FEATURE_GROUP_PREDICTIONS_METADATA = FeatureGroupConfig(
     name='model_predictions_feature_group',
-    version=1,
+    version=7,
     description="Predictions generate by our production model",
-    # primary_key = ['pickup_location_id', 'pickup_ts'],
-    primary_key = ['pickup_location_id'],
+    primary_key = ['pickup_location_id', 'pickup_ts'],
+    # primary_key = ['pickup_location_id'],
     event_time='pickup_ts',
 )
 
@@ -55,12 +55,12 @@ FEATURE_GROUP_PREDICTIONS_METADATA = FeatureGroupConfig(
 FEATURE_VIEW_MODEL_PREDICTIONS = 'model_predictions_feature_view'
 FEATURE_VIEW_PREDICTIONS_METADATA = FeatureViewConfig(
     name='model_predictions_feature_view',
-    version=5,
+    version=7,
     feature_group=FEATURE_GROUP_PREDICTIONS_METADATA,
 )
 
 MONITORING_FV_NAME = 'monitoring_feature_view'
-MONITORING_FV_VERSION = 5
+MONITORING_FV_VERSION = 7
 
 # number of historical values our model needs to generate predictions
 N_FEATURES = 24 * 28
